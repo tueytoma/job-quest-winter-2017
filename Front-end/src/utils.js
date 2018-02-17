064c2c5200525c5379e033d1ad4d6957ffd0f851
tueytoma <tueytobi@gmail.com>
2018-02-17 18:28:42 +0700

    add utils

--- Front-end/src/utils.js
+++ Front-end/src/utils.js
@@ -1,3 +1,11 @@
 let utils = {}
 
+utils.isMobile = () => {
+	//if(!navigator) return false
+	// return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+	return /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+		navigator.userAgent
+	)
+}
+
 module.exports = utils
